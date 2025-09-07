8facc364c95797f85372a96ef466bb3745329fac
Dreknor <Dreknor@users.noreply.github.com>
2025-09-07 09:03:20 +0200

    Update ergebnis.blade.php and ErgebnisController.php to display results for multiple VK numbers

--- app/Http/Controllers/ErgebnisController.php
+++ app/Http/Controllers/ErgebnisController.php
@@ -8,21 +8,16 @@ use App\Model\settings;
 use App\Repositories\Klamottenboerse\KlamottenboersenRepository;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
+use App\Model\VKnummer;
 
 class ErgebnisController extends Controller
 {
-
-    public function __construct()
-    {
-
-    }
+    public function __construct() { }
 
     public function index() {
-
         if (!(new KlamottenboersenRepository())->aktuelleKlamottenboerse()->ergebnis_freigabe) {
             abort(404);
         }
-
         return view('ergebnis.index');
     }
 
@@ -30,57 +25,35 @@ class ErgebnisController extends Controller
         if (!(new KlamottenboersenRepository())->aktuelleKlamottenboerse()->ergebnis_freigabe) {
             abort(404);
         }
-
-        $request->validate([
-            'email' => 'required|email',
-        ]);
-
+        $request->validate(['email' => 'required|email']);
         try {
             $interessent = Interessenten::query()->where('mail', $request->input('email'))->first();
-            if ($interessent){
+            if ($interessent) {
                 Mail::to($interessent->mail)->send(new ErgebnisLinkMail($interessent));
             }
         } catch (\Exception $e) {
-            return redirect()->route('ergebnis')->with([
-                'message' => 'Es ist ein Fehler aufgetreten. Bitte versuchen Sie es später erneut.'
-            ]);
+            return redirect()->route('ergebnis')->with(['message' => 'Es ist ein Fehler aufgetreten. Bitte versuchen Sie es später erneut.']);
         }
-
-
-        return redirect()->route('ergebnis')->with([
-            'message' => 'Sollte die E-Mail-Adresse in unserer Datenbank vorhanden sein, haben wir Ihnen den Link zum Ergebnis zugesendet.'
-        ]);
-
+        return redirect()->route('ergebnis')->with(['message' => 'Sollte die E-Mail-Adresse in unserer Datenbank vorhanden sein, haben wir Ihnen den Link zum Ergebnis zugesendet.']);
     }
 
     public function show($uuid) {
-        if (!(new KlamottenboersenRepository())->aktuelleKlamottenboerse()->ergebnis_freigabe) {
-            abort(404);
-        }
 
         $interessent = Interessenten::query()->where('uuid', $uuid)->first();
-        if ($interessent) {
-           $vknummer = $interessent->vknummern_vergeben;
+        if (!$interessent) {
+            return redirect()->route('ergebnis')->with(['message' => 'Der Link ist ungültig.']);
+        }
+        // Alle freigegebenen Ergebnisse (auch vergangene) sammeln
+        $vknummern = $interessent->bisherige_vknummen()->limit(5)->get();
 
-           $klamottenboerse = (new KlamottenboersenRepository())->aktuelleKlamottenboerse();
 
-           if ($vknummer && $vknummer->klamottenboersen_id == $klamottenboerse->id) {
-             return view('ergebnis.ergebnis', [
-                    'interessent' => $interessent,
-                    'vknummer' => $vknummer,
-                    'klamottenboerse' => $klamottenboerse,
-                    'einbehalt' => settings::query()->latest()->first()->provision
-             ]);
-              } else {
-                return redirect()->route('ergebnis')->with([
-                    'message' => 'Für diese Klamottenbörse liegt kein Ergebnis vor.'
-                ]);
-            }
-
-        } else {
-            return redirect()->route('ergebnis')->with([
-                'message' => 'Der Link ist ungültig.'
+        //dd($vknummern);
+        if ($vknummern->count() > 0) {
+            return view('ergebnis.ergebnis', [
+                'interessent' => $interessent,
+                'vknummernFreigegeben' => $vknummern,
             ]);
         }
+        return redirect()->route('ergebnis')->with(['message' => 'Für diese Klamottenbörse liegt kein Ergebnis vor.']);
     }
 }
--- resources/views/ergebnis/ergebnis.blade.php
+++ resources/views/ergebnis/ergebnis.blade.php
@@ -2,71 +2,73 @@
 
 @section('content')
     <div class="container-fluid">
-        <div class="card">
-            <div class="card-header bg-info text-white">
-                <h3>
-                    Verkaufte Artikel zur Klamottenbörse am {{ $klamottenboerse->datum->format('d.m.Y') }}
-                </h3>
-            </div>
-            <div class="card-body">
-                <p>
-                    Hier sehen Sie die Artikel, die verkauft wurden.
-                </p>
+        @foreach($vknummernFreigegeben as $vknummer )
+            <div class="card mb-3">
+                <div class="card-header">
+                    <h3>
+                        Ergebnis für VK-Nummer {{ $vknummer->vk_nummer }} zur Klamottenbörse am {{ $vknummer->klamottenboerse->datum->format('d.m.Y') }}
+                    </h3>
+                </div>
+                <div class="card-body">
+                    <p>
+                        Hier sehen Sie die Artikel, die verkauft wurden.
+                    </p>
 
-                <div class="table-md-responsive">
-                    <table class="table table-bordered table-striped">
-                        <thead>
-                            <tr>
-                                <th>
-                                    Artikel
-                                </th>
-                                <th>
-                                    Preis
-                                </th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach($vknummer->verkaufteArtikel->sortBy('artikelnummer') as $artikel)
+                    <div class="table-md-responsive">
+                        <table class="table table-bordered table-striped">
+                            <thead>
                                 <tr>
-                                    <td>
-                                        {{ $artikel->artikelnummer }}
-                                    </td>
-                                    <td>
-                                        {{ number_format($artikel->betrag, 2, ',', '.') }} €
-                                    </td>
+                                    <th>
+                                        Artikel
+                                    </th>
+                                    <th>
+                                        Preis
+                                    </th>
                                 </tr>
-                            @endforeach
-                        </tbody>
-                        <tfoot class="bg-info text-white">
+                            </thead>
+                            <tbody>
+                                @foreach($vknummer->verkaufteArtikel->sortBy('artikelnummer') as $artikel)
+                                    <tr>
+                                        <td>
+                                            {{ $artikel->artikelnummer }}
+                                        </td>
+                                        <td>
+                                            {{ number_format($artikel->betrag, 2, ',', '.') }} €
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                            <tfoot class="bg-info text-white">
+                                <tr>
+                                    <th>
+                                        Summe:
+                                    </th>
+                                    <th>
+                                        {{ number_format($vknummer->verkaufteArtikel->sum('betrag'), 2, ',', '.') }} €
+                                    </th>
+                                </tr>
+                                <tr>
+                                    <th>
+                                        Davon 25%:
+                                    </th>
+                                    <th>
+                                        {{ number_format($vknummer->verkaufteArtikel->sum('betrag') / 100 * 25, 2, ',', '.') }} €
+                                    </th>
+                                </tr>
                             <tr>
                                 <th>
-                                    Summe:
+                                    Auszahlungsbetrag:
                                 </th>
                                 <th>
-                                    {{ number_format($vknummer->verkaufteArtikel->sum('betrag'), 2, ',', '.') }} €
+                                    {{ number_format($vknummer->verkaufteArtikel->sum('betrag') - $vknummer->verkaufteArtikel->sum('betrag') / 100 * 25, 2, ',', '.') }} €
                                 </th>
                             </tr>
-                            <tr>
-                                <th>
-                                    Davon {{$einbehalt}}%:
-                                </th>
-                                <th>
-                                    {{ number_format($vknummer->verkaufteArtikel->sum('betrag') / 100 * $einbehalt, 2, ',', '.') }} €
-                                </th>
-                            </tr>
-                        <tr>
-                            <th>
-                                Auszahlungsbetrag:
-                            </th>
-                            <th>
-                                {{ number_format($vknummer->verkaufteArtikel->sum('betrag') - $vknummer->verkaufteArtikel->sum('betrag') / 100 * $einbehalt, 2, ',', '.') }} €
-                            </th>
-                        </tr>
-                        </tfoot>
+                            </tfoot>
 
-                    </table>
+                        </table>
+                    </div>
                 </div>
             </div>
-        </div>
+        @endforeach
     </div>
 @endsection
